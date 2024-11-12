d271e71cba4620d21a757cd5d2af44c10c86c018
София Корватовская <korvatovskaya.s@skbkontur.ru>
2024-11-12 12:43:17 +0500

    deprecate models and methods

--- src/main/java/Diadoc/Api/sign/SignClient.java
+++ src/main/java/Diadoc/Api/sign/SignClient.java
@@ -27,6 +27,10 @@ public class SignClient {
         return cloudSign(request, null);
     }
 
+    /**
+     * @deprecated Этот метод будет удалён в следующей мажорной версии.
+     * Для сценариев с подписаниями используйте методы: dssSign и dssSignResult
+     */
     public AsyncMethodResult cloudSign(CloudSignRequest cloudSignRequest, @Nullable String certificateThumbprint) throws DiadocSdkException {
         if (cloudSignRequest == null) {
             throw new IllegalArgumentException("cloudSignRequest");
@@ -45,6 +49,10 @@ public class SignClient {
         }
     }
 
+    /**
+     * @deprecated Этот метод будет удалён в следующей мажорной версии.
+     * Для сценариев с подписаниями используйте методы: {@link #dssSign(String, DssSignRequest)} и {@link #dssSignResult(String, String)}
+     */
     public CloudSignResult waitCloudSignResult(String taskId, Integer timeoutInMillis) throws DiadocSdkException {
         try {
             var data = diadocHttpClient.waitTaskResult("/CloudSignResult", taskId, timeoutInMillis);
@@ -54,6 +62,10 @@ public class SignClient {
         }
     }
 
+    /**
+     * @deprecated Этот метод будет удалён в следующей мажорной версии.
+     * Для сценариев с подписаниями используйте методы: {@link #dssSign(String, DssSignRequest)} и {@link #dssSignResult(String, String)}
+     */
     public AsyncMethodResult cloudSignConfirm(String token, String confirmationCode, boolean returnContent) throws DiadocSdkException {
         try {
             var url = new URIBuilder(diadocHttpClient.getBaseUrl())
@@ -70,6 +82,10 @@ public class SignClient {
         }
     }
 
+    /**
+     * @deprecated Этот метод будет удалён в следующей мажорной версии.
+     * Для сценариев с подписаниями используйте методы: {@link #dssSign(String, DssSignRequest)} и {@link #dssSignResult(String, String)}
+     */
     public CloudSignConfirmResult waitCloudSignConfirmResult(String taskId, Integer timeoutInMillis) throws DiadocSdkException {
         try {
             byte[] data = diadocHttpClient.waitTaskResult("/CloudSignConfirmResult", taskId, timeoutInMillis);
